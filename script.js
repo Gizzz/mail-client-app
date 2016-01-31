25289a09b0dff13337cc43644068d1f76bc555dc
Gizzz <bulatkhusainov@yandex.ru>
2016-01-31 20:55:02 +0300

    make email data fetch from server

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ angular.module("email-client-app").directive("folderList", function() {
           self.folders = result;
         },
         function(error) {
-          self.folders = [{ name: "Error occurred while fetching folders.", count: 0 }];
+          self.folders = [{ name: "Error occurred while fetching data.", count: 0 }];
           console.log(error);
         }
       );
@@ -41,8 +41,19 @@ angular.module("email-client-app").directive("emailList", function() {
     restrict: "E",
     templateUrl: "templates/email-list.html",
     controllerAs: "emailListCtrl",
-    controller: function(emailListStorage, globalState) {
-      this.emailsByFolder = emailListStorage.emailsByFolder;
+    controller: function(emailStorage, globalState) {
+      this.emailsByFolder = { "incoming": [{"subject": "fetching data..." }] };
+      var self = this;
+
+      emailStorage.getEmailsByFolderPromise().then(
+        function(result) {
+          self.emailsByFolder = result;
+        },
+        function(error) {
+          self.folders = { "incoming": [{"subject": "Error occurred while fetching data." }] };
+          console.log(error);
+        }
+      );
       
       this.getActiveFolder = function() {
         return globalState.activeFolder;
@@ -73,89 +84,38 @@ angular.module("email-client-app").directive("emailDetails", function() {
 // -----------------------
 
 angular.module("email-client-app").factory("emailStorage", function($http) {
-  var emailsByFolder = undefined;
-
-
+  var emailsByFolderPromise = $http.get("https://jsonblob.com/api/jsonBlob/56ad0df8e4b01190df4c3fb3");
 
   return {
-    emailsByFolder: emailsByFolder,
+    getEmailsByFolderPromise: function() {
+      var resultPromise = emailsByFolderPromise.then(function (result) {
+        return result.data;
+      });
 
+      return resultPromise;
+    },
     getFoldersPromise: function() {
-      var promise = $http.get("https://jsonblob.com/api/jsonBlob/56ad0df8e4b01190df4c3fb3")
-        .then(function (result) {
-          var emailsByFolder = result.data;
-          var folderNames = Object.keys(emailsByFolder);
-          var folders = [];
+      var foldersPromise = emailsByFolderPromise.then(function (result) {
+        var emailsByFolder = result.data;
+        var folderNames = Object.keys(emailsByFolder);
+        var folders = [];
 
-          folderNames.forEach(function(key) {
-            folders.push({name: key, count: emailsByFolder[key].length});
-          });
-
-          return folders;
+        folderNames.forEach(function(key) {
+          folders.push({name: key, count: emailsByFolder[key].length});
         });
 
-      return promise;
+        return folders;
+      });
+
+      return foldersPromise;
     },
   };
 });
 
-// to remove
-angular.module("email-client-app").factory("emailListStorage", function($http) {
-  var emailsByFolder = {
-    incoming: [
-      { 
-        from: "John", 
-        subject: "Hi, how was your vacation? ", 
-        get text() { return this.subject.repeat(10); },
-      }, { 
-        from: "Sam", 
-        subject: "Need more coffee ", 
-        get text() { return this.subject.repeat(10); },
-      }, { 
-        from: "Gabe", 
-        subject: "Win 10 updates", 
-        get text() { return this.subject.repeat(10); },
-      },
-    ],
-    sent: [
-      { 
-        from: "Carl", 
-        subject: "Miranda - salary ", 
-        get text() { return this.subject.repeat(10); },
-      }, { 
-        from: "Fred", 
-        subject: "What about holidays? ", 
-        get text() { return this.subject.repeat(10); },
-      }, 
-    ],
-    trash: [
-      { 
-        from: "Спортмастер", 
-        subject: "Зимняя распродажа! ", 
-        get text() { return this.subject.repeat(10); },
-      },
-    ],
-  };
-  
-  return {
-    emailsByFolder: emailsByFolder,
-    get folders() {
-      var folderNames = Object.keys(this.emailsByFolder);
-      
-      var folders = []; 
-      folderNames.forEach(function(key) {
-        folders.push({name: key, count: emailsByFolder[key].length});
-      });
-      
-      return folders;
-    },
-  };
-});
-  
 angular.module("email-client-app").factory("globalState", function() {
   var globalState = {
     activeFolder: "incoming",
-    activeEmail: undefined,
+    activeEmail: { text: "Choose item in email list..." },
   };
 
   return globalState;
