a7be6351104535a056269f9ce2bb0d8c511aa9e6
Gizzz <bulatkhusainov@yandex.ru>
2016-02-21 21:59:06 +0300

    add tests: userStorage

--- js/contacts_test.js
+++ js/contacts_test.js
@@ -1,5 +1,16 @@
+"use strict";
+
 describe("userStorage", function() {
-	it("should return true", function () {
-		expect(true).toEqual(true);
+	var userStorage;
+
+	beforeEach(module("email-client-app"));
+	beforeEach(function() {
+		inject(function($injector) {
+			userStorage = $injector.get("userStorage");
+		})
+	});
+
+	it("should has 3 users", function () {
+		expect(userStorage.getUsers().length).toBe(3);
 	});
 });
